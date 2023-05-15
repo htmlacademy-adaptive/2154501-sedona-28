2e7f803351e6779ba67a504528e6b9e3b6b359d8
Randell87 <randell@mail.ru>
2023-05-15 20:00:07 +0700

    тест

--- gulpfile.js
+++ gulpfile.js
@@ -68,6 +68,7 @@ const server = (done) => {
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
+
 }
 
 
